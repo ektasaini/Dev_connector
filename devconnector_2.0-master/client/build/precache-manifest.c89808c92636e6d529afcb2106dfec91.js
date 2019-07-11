cb86359de5db31ce84ee1fc8432c132cf5883095
ektasaini <ektasaini1312.com@gmail.com>
2019-07-11 23:47:36 +0530

    dev_connector

---
+++ devconnector_2.0-master/client/build/precache-manifest.c89808c92636e6d529afcb2106dfec91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c84c33e0abfcf7c8ea42a96af55d6cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6bcfd219b4ec188337",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "36c68d140a8553eebb51",
+    "url": "/static/js/2.f738b9b9.chunk.js"
+  },
+  {
+    "revision": "0c6bcfd219b4ec188337",
+    "url": "/static/js/main.d8f8ef14.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
